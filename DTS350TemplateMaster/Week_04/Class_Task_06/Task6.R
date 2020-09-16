08e55016855eb2151729258478b691ef9b8b9204
bradymonks <bradymonks@gmail.com>
2020-09-15 22:49:16 -0500

    Task 6

--- DTS350TemplateMaster/Week_04/Class_Task_06/Task6.R
+++ DTS350TemplateMaster/Week_04/Class_Task_06/Task6.R
@@ -3,35 +3,43 @@
 library(tidyverse)
 
 
-Task6Data <- read_csv("task6data.csv")
+data1 <- read_csv("life-expectancy-vs-gdp-per-capita.csv")
 
-Task6Data
+view(data1)
 
-colnames(Task6Data)
+data2015 <- filter(data1, Year == 2015)
+view(data2015)
+
+data2015 <- data2015 %>%
+  rename(
+    GDP_Per_Capita_USD = "Real GDP per capita in 2011US$, multiple benchmarks (Maddison Project Database (2018))",
+    Life_expectancy = "Life expectancy",
+    Total_Population = "Total population (Gapminder, HYDE & UN)",
+    Country = "Entity"
+  )
+str(data2015)
+
+data2015v2 <- na.omit(data2015)
+
+view(data2015v2)
+
+ggplot(data = data2015v2)+
+  geom_point(mapping = aes(x = GDP_Per_Capita_USD, y = Life_expectancy, size = Total_Population/10000, color = Continent,))+
+  labs(x = "GDP per capita",
+       y = "Life expectancy at birth",
+       title = "Life expectancy vs. GDP per capita, 2015",
+       subtitle = "GDP per capita is measured in 2011 international dollars, which corrects for inflation and cross-country price differences.")+  
+  guides(size = FALSE)+
+  scale_x_continuous(breaks = seq(0,120000, by = 20000),labels=scales::dollar_format())+
+  theme(plot.title = element_text(hjust = 0))
+  
 
 
-Task6Data <- Task6Data %>%
-              rename(
-                GDP_Per_Capita_USD = "GDP per capita (current US$)",
-                Food_Expenditure_USDA_2017 = "Food expenditure (USDA (2017))",
-                Total_Population = "Total population (Gapminder"
-              )
-
-colnames(Task6Data)
 
 
-ggplot(data = Task6Data)+
-    geom_point(mapping = aes(x = GDP_Per_Capita_USD, y = Food_Expenditure_USDA_2017/Total_Population, color = Continent, size = Total_Population))
-Task6Data$Food_Expenditure_USDA_2017
-
-Task6Data1 <- read_csv("GDP_per.csv")
-
-Task6_2015 <- filter(Task6Data1,
-                      Year == 2015)
-view(Task6_2015)
 
 
-view(Task6_2015)
-colnames(Task6Data1)
-ggplot(data = Task6_2015)+
-  geom_point(mapping = aes(x = GDP_Per_Capita_USD, y = Food_Expenditure_USDA_2017/Total_Population, color = Continent, size = Total_Population/100000))
+
+
+
+
